118e1d2939b96d69f6a9479b57f54a625a738d31
longjing <longjing@focusmedia.cn>
2019-04-23 21:11:18 +0800

    sourter dnd

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import DND from './component/dnd/index';
+import DND from './component/dndSorter';
 import './App.css';
 
 class App extends Component {
@@ -8,19 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <DND />
-          {/*<a*/}
-            {/*className="App-link"*/}
-            {/*href="https://reactjs.org"*/}
-            {/*target="_blank"*/}
-            {/*rel="noopener noreferrer"*/}
-          {/*>*/}
-            {/*Learn React*/}
-          {/*</a>*/}
         </header>
       </div>
     );
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-//
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-//
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Board from './component/dnd/board';
-import { observe } from './component/dnd/game'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const root = document.getElementById('root')
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-observe(knightPosition =>
-    ReactDOM.render(<Board knightPosition={knightPosition} />, root),
-)
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import Board from './component/dnd/board';
+// import { observe } from './component/dnd/game'
+//
+// const root = document.getElementById('root')
+//
+// observe(knightPosition =>
+//     ReactDOM.render(<Board knightPosition={knightPosition} />, root),
+// )
 
 // import React, { useEffect, useState } from 'react'
 // import Board from './component/dnd/board'
