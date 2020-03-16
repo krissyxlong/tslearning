5d58b80415bea3eaf220ec31a5cff8fb203bc532
longjing <longjing@focusmedia.cn>
2020-03-16 22:26:41 +0800

    add aop closure throttle

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import useWindowWidth from './component/hooks/useWindowWidth';
-import Context from './component/hooks/context';
-import SetInterval, { Step } from './component/hooks/setInterval';
-import Video from './component/video';
+import JsAlgorithms from './component/alg/jsAlgorithms';
 import './App.css';
 
 const App = () => {
-    // const width = useWindowWidth();
     return <div className="App">
         <div id='hello'>hello world</div>
-        {/*<p>窗口宽度：{width}</p>*/}
-        {/*<Context />*/}
-        {/*<SetInterval />*/}
-        {/*<Step />*/}
-        <Video />
     </div>;
 };
 
