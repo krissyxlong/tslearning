14469928d4265a3f5fa411b141aa7cbf24b1aadb
longjing <longjing@focusmedia.cn>
2020-03-21 16:55:29 +0800

    add design test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import JsAlgorithms from './component/alg/jsAlgorithms';
 import './App.css';
-
 const App = () => {
     return <div className="App">
-        <div id='hello'>hello world</div>
+        <div id='loginBtn'>hello world</div>
     </div>;
 };
 
