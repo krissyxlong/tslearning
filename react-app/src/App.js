9bdd461311989a922f528cbeb5a01992f731fbd3
longjing <longjing@focusmedia.cn>
2020-04-01 18:21:06 +0800

    add promise

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,4 @@
-/*
- * @Description: 
- * @Author: 
- * @Date: 2020-01-10 16:34:38
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-03-30 18:37:37
- */
+
 import React from 'react';
 import Demo from './component/hooks/demo';
 import './App.css';
