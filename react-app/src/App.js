d050d7d29d5e637fe4722669d7c2af9f7000fec5
longjing <longjing@focusmedia.cn>
2020-04-09 15:17:25 +0800

    add context component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,12 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Ref from './component/ref';
+import ContextDemo from './component/context';
 
 const App = () => {
     return <div className="App">
         <div id='loginBtn'>hello world</div>
-        <Ref />
+        <ContextDemo />
     </div>;
 };
 
